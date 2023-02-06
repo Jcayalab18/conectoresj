a539290d0d446ff9ca8df3d6960f76f35f169e3c
Jose Carlos <4192287@alu.murciaeduca.es>
2023-02-06 21:42:24 +0100

    Proyecto creado

---
+++ src/main/java/es/eldelbit/conectoresj/Conectoresj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.eldelbit.conectoresj;
+
+/**
+ *
+ * @author alex
+ */
+public class Conectoresj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
